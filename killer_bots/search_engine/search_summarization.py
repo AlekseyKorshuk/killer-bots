e70b8cab9e4d08b8aec9ca39236b885fff5cf5bb
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-26 22:05:34 +0300

    updates

--- killer_bots/search_engine/search_summarization.py
+++ killer_bots/search_engine/search_summarization.py
@@ -31,7 +31,7 @@ def get_retriever(doc_dir):
 
 
 def test_retriever():
-    retriever = get_retriever("../bots/code_guru/database")
+    retriever = get_retriever("/app/killer-bots/bots/code_guru/database")
     p_retrieval = DocumentSearchPipeline(retriever)
     res = p_retrieval.run(query="What is SOLID?", params={"Retriever": {"top_k": 3}})
     print_documents(res, max_text_len=512)
