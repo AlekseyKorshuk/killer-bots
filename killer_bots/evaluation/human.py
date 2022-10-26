1cb692b1e437f70772841c345bb2051ef333657b
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-26 14:28:59 +0300

    updates

--- killer_bots/evaluation/human.py
+++ killer_bots/evaluation/human.py
@@ -14,7 +14,8 @@ import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-MODEL = 'facebook/opt-125m'
+# MODEL = 'facebook/opt-125m'
+MODEL = 'EleutherAI/gpt-j-6B'
 # MODEL = 'facebook/opt-30b'
 
 REWARD_MODEL = "ChaiML/roberta-base-dalio-reg-v1"
