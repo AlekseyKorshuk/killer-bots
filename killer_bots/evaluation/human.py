cfd533056bcb110e4dddaf1edb4d193e1fdae581
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-27 23:28:33 +0300

    updates

--- killer_bots/evaluation/human.py
+++ killer_bots/evaluation/human.py
@@ -60,7 +60,7 @@ params = {
     "top_k": 20,
     "temperature": 1.0,
     "repetition_penalty": 1.0,
-    "eos_token_id": 50118,  # 50118
+    # "eos_token_id": 50118,  # 50118
     "device": device,
     "do_sample": True,
     "max_new_tokens": 256,
