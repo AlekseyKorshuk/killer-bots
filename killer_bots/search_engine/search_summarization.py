185d3c812f7d8324e192bd31bf98c6974c25308c
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-27 16:29:38 +0300

    git pull

--- killer_bots/search_engine/search_summarization.py
+++ killer_bots/search_engine/search_summarization.py
@@ -15,7 +15,7 @@ logging.getLogger("haystack").setLevel(logging.INFO)
 
 def get_document_store():
     os.remove("faiss_document_store.db")
-    document_store = FAISSDocumentStore(embedding_dim=1024, faiss_index_factory_str="Flat")
+    document_store = FAISSDocumentStore(embedding_dim=128, faiss_index_factory_str="Flat")
     return document_store
 
 
@@ -32,8 +32,8 @@ def get_retriever(doc_dir):
     document_store = write_docs(document_store, doc_dir)
     retriever = DensePassageRetriever(
         document_store=document_store,
-        query_embedding_model="CarperAI/carptriever-1",
-        passage_embedding_model="CarperAI/carptriever-1",
+        query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+        passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
     )
     document_store.update_embeddings(retriever)
     return retriever
