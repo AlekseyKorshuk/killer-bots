18da0c293a085f97972262fee1c5f8837bba8360
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-27 17:14:08 +0300

    git pull

--- killer_bots/bots/code_guru/prompts.py
+++ killer_bots/bots/code_guru/prompts.py
@@ -1,7 +1,6 @@
 START_PROMPT = """This is a conversation where Coding Guru is giving advice on being a developer and helping with any questions. Guru uses Context from books to answer User's questions.
 
-User: Hi Guru, thanks for talking with me today. I am excited to learn more about how to be a good developer and solve some problems.
-Guru: """
+User: Hi Guru, thanks for talking with me today. I am excited to learn more about how to be a good developer and solve some problems."""
 
 SOLID_CONTEXT = """SOLID Principles is a coding standard that all developers should have a clear concept for developing software properly to avoid a bad design. SOLID stands for: S - Single-responsibility Principle, O - Open-closed Principle, L - Liskov Substitution Principle, I - Interface Segregation Principle, D - Dependency Inversion Principle."""
 
