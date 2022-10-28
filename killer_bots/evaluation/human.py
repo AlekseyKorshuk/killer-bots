7bd41393db27ae29894b0f792d9882facd662ce9
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-28 18:06:40 +0300

    updates

--- killer_bots/evaluation/human.py
+++ killer_bots/evaluation/human.py
@@ -56,10 +56,10 @@ def load_huggingface_model(model_id):
 
 
 params = {
-    "top_p": 1,
+    "top_p": 0,
     "top_k": 12,
-    "temperature": 1.0,
-    "repetition_penalty": 1.0,
+    "temperature": 0.9910335549814574,
+    "repetition_penalty": 1.0420516045705377,
     # "eos_token_id": 50118,  # 50118
     "device": device,
     "do_sample": True,
