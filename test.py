88e992c517e8143c847d472c36bc6028e24d216c
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-30 23:35:54 +0300

    updates

--- test.py
+++ test.py
@@ -14,6 +14,52 @@ DOCUMENT = re.sub(r'\n|\r', ' ', DOCUMENT)
 DOCUMENT = re.sub(r' +', ' ', DOCUMENT)
 DOCUMENT = DOCUMENT.strip()
 
+def nest_sentences(document):
+
+  nested = []
+  sent = []
+  length = 0
+  for sentence in nltk.sent_tokenize(document):
+    length += len(sentence)
+    if length < 1024:
+      sent.append(sentence)
+    else:
+      nested.append(sent)
+      sent = []
+      length = 0
+
+  if sent:
+    nested.append(sent)
+
+  return nested
+
+nested = nest_sentences(DOCUMENT)
+
+
+from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
+
+BART_PATH = 'facebook/bart-large-cnn'
+
+bart_model = BartForConditionalGeneration.from_pretrained(BART_PATH, output_past=True)
+bart_tokenizer = BartTokenizer.from_pretrained(BART_PATH, output_past=True)
+
+def generate_summary(nested_sentences):
+  device = 'cuda'
+  summaries = []
+  for nested in nested_sentences:
+    input_tokenized = bart_tokenizer.encode(' '.join(nested), truncation=True, return_tensors='pt')
+    input_tokenized = input_tokenized.to(device)
+    summary_ids = bart_model.to('cuda').generate(input_tokenized,
+                                      length_penalty=3.0,
+                                      min_length=30,
+                                      max_length=100)
+    output = [bart_tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
+    summaries.append(output)
+  summaries = [sentence for sublist in summaries for sentence in sublist]
+  return summaries
+
+
 sm = Summarizer(model='distilbert-base-uncased')
 result = sm(body=DOCUMENT, ratio=0.15)
 print(result)
+
