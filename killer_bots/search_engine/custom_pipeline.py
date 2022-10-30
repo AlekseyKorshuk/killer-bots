3a878701f3ef0fe3d285294218e377682246bd5b
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-30 19:11:03 +0300

    updates

---
+++ killer_bots/search_engine/custom_pipeline.py
@@ -0,0 +1,126 @@
+import os
+
+from haystack.document_stores import FAISSDocumentStore
+from haystack.nodes import PreProcessor, EmbeddingRetriever
+from haystack.pipelines import DocumentSearchPipeline
+from haystack.utils import convert_files_to_docs, clean_wiki_text
+from transformers import AutoTokenizer
+
+from killer_bots.search_engine.utils import change_extentions_to_txt
+
+
+def _get_document_store():
+    try:
+        os.remove("faiss_document_store.db")
+    except:
+        pass
+    document_store = FAISSDocumentStore(
+        sql_url="sqlite:///faiss_document_store.db",
+        # embedding_dim=128,
+        faiss_index_factory_str="Flat",
+        return_embedding=True
+    )
+    return document_store
+
+
+def get_documents(doc_dir):
+    change_extentions_to_txt(doc_dir)
+    docs = convert_files_to_docs(dir_path=doc_dir, clean_func=clean_wiki_text, split_paragraphs=False)
+
+    preprocessor = PreProcessor(
+        clean_empty_lines=True,
+        clean_whitespace=True,
+        clean_header_footer=False,
+        split_by="word",
+        split_length=100,
+        split_respect_sentence_boundary=True,
+    )
+    docs = preprocessor.process(docs)
+    return docs
+
+
+def get_retriever(document_store):
+    retriever = EmbeddingRetriever(
+        document_store=document_store,
+        embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1",
+        model_format="sentence_transformers"
+    )
+    return retriever
+
+
+def get_file_scores(documents):
+    stats = {}
+    counter = {}
+    for doc in documents:
+        if doc.meta["name"] not in stats:
+            stats[doc.meta["name"]] = 0
+            counter[doc.meta["name"]] = 0
+        stats[doc.meta["name"]] += doc.score
+        counter[doc.meta["name"]] += 1
+    for key in stats:
+        stats[key] /= counter[key]
+    # sort by score descending order
+    stats = {k: v for k, v in sorted(stats.items(), key=lambda item: item[1], reverse=True)}
+    return stats
+
+
+def get_top_docs(documents, stats, top_k=5):
+    top_files = list(stats.keys())[:top_k]
+    top_docs = []
+    for doc in documents:
+        if doc.meta["name"] in top_files:
+            top_docs.append(doc)
+    top_docs = top_docs[:top_k]
+    return top_docs
+
+
+def postprocess_top_docs(top_docs):
+    top_docs = sorted(top_docs, key=lambda x: x.meta['vector_id'], reverse=False)
+    top_docs = sorted(top_docs, key=lambda x: x.meta['name'], reverse=False)
+    return top_docs
+
+
+def postprocess_answer(top_docs):
+    joined_text = "\n".join([doc.content for doc in top_docs])
+    return joined_text
+
+
+class Pipeline:
+    def __init__(self, doc_dir):
+        self.document_store = _get_document_store()
+        docs = get_documents(doc_dir)
+        self.num_docs = len(docs)
+        self.document_store.write_documents(docs)
+        self.retriever = get_retriever(self.document_store)
+        self.document_store.update_embeddings(self.retriever)
+        self.document_search_pipeline = DocumentSearchPipeline(self.retriever)
+        self.tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b")
+
+    def get_relevant_docs(self, query, top_k):
+        documents = self.document_search_pipeline.run(
+            query=query,
+            params={
+                "Retriever": {
+                    "top_k": self.num_docs
+                }
+            }
+        )["documents"]
+        return documents
+
+    def print_logs(self, query, top_docs, answer):
+        print("Query:", query)
+        print("Docs:")
+        for doc in top_docs:
+            print(doc.meta["name"], doc.score)
+        num_tokens = len(self.tokenizer(answer).input_ids)
+        print(f"Number of tokens: {num_tokens}")
+
+    def __call__(self, query, top_k=5, verbose=False):
+        documents = self.get_relevant_docs(query, self.num_docs)
+        stats = get_file_scores(documents)
+        top_docs = get_top_docs(documents, stats, top_k)
+        top_docs = postprocess_top_docs(top_docs)
+        answer = postprocess_answer(top_docs)
+        if verbose:
+            self.print_logs(query, top_docs, answer)
+        return answer
