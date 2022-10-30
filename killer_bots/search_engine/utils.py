312e52b2277d1e3b2e2bb823f00cc2d66931bcd7
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-30 14:06:13 +0300

    updates

--- killer_bots/search_engine/utils.py
+++ killer_bots/search_engine/utils.py
@@ -3,9 +3,9 @@ from haystack.document_stores import FAISSDocumentStore
 import numpy as np
 from haystack.utils import convert_files_to_docs, fetch_archive_from_http, clean_wiki_text, print_documents
 from haystack.nodes import DensePassageRetriever, TransformersSummarizer, Seq2SeqGenerator, PreProcessor, RAGenerator, \
-    BM25Retriever
+    BM25Retriever, SentenceTransformersRanker
 from haystack.pipelines import DocumentSearchPipeline, SearchSummarizationPipeline, GenerativeQAPipeline
-from haystack import Document
+from haystack import Document, Pipeline
 from sklearn.metrics.pairwise import cosine_similarity
 import urllib.request
 
@@ -50,13 +50,13 @@ def get_retriever(doc_dir):
     #     use_gpu=True,
     #     embed_title=True,
     # )
-    # retriever = DensePassageRetriever(
-    #     document_store=document_store,
-    #     query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
-    #     passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
-    # )
-    retriever = BM25Retriever(document_store=document_store)
-    # document_store.update_embeddings(retriever)
+    retriever = DensePassageRetriever(
+        document_store=document_store,
+        query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+        passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+    )
+    # retriever = BM25Retriever(document_store=document_store)
+    document_store.update_embeddings(retriever)
     return retriever
 
 
@@ -76,6 +76,7 @@ def get_search_summarization_pipeline(doc_dir):
 def get_lfqa_generator():
     generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
     return generator
+
 
 def get_rag_generator():
     generator = RAGenerator(
@@ -128,7 +129,31 @@ def get_huggingface_course_docs():
     return docs
 
 
-if __name__ == "__main__":
-    docs = get_huggingface_course_docs()
+def test():
+    doc_dir = "/app/killer-bots/killer_bots/bots/code_guru/database"
+    os.remove("faiss_full_document_store.db")
+    document_store = FAISSDocumentStore(
+        sql_url="sqlite:///faiss_full_document_store.db",
+        embedding_dim=128,
+        faiss_index_factory_str="Flat",
+        return_embedding=True
+    )
+    change_extentions_to_txt(doc_dir)
+    docs = convert_files_to_docs(dir_path=doc_dir, clean_func=None, split_paragraphs=False)
     print(f"Number of docs: {len(docs)}")
-    print(docs[:5])
+    # import pdb; pdb.set_trace()
+    document_store.write_documents(docs)
+
+    retriever = DensePassageRetriever(
+        document_store=document_store,
+        query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
+        passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+    )
+    document_store.update_embeddings(retriever)
+    p_retrieval = DocumentSearchPipeline(retriever)
+    res = p_retrieval.run(query="Tell me something about SOLID?", params={"Retriever": {"top_k": 10}})
+    print_documents(res, max_text_len=512)
+
+
+if __name__ == "__main__":
+    test()
