5dc12d20d50a3fd21ab4c0873b07bf82df9f16ba
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-30 19:24:46 +0300

    updates

--- killer_bots/search_engine/custom_pipeline.py
+++ killer_bots/search_engine/custom_pipeline.py
@@ -3,10 +3,40 @@ import os
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import PreProcessor, EmbeddingRetriever
 from haystack.pipelines import DocumentSearchPipeline
-from haystack.utils import convert_files_to_docs, clean_wiki_text
+from haystack.utils import convert_files_to_docs
 from transformers import AutoTokenizer
 
 from killer_bots.search_engine.utils import change_extentions_to_txt
+
+import re
+
+
+def clean_wiki_text(text: str) -> str:
+    # get rid of multiple new lines
+    while "\n\n" in text:
+        text = text.replace("\n\n", "\n")
+
+    # remove extremely short lines
+    lines = text.split("\n")
+    cleaned = []
+    for l in lines:
+        if len(l) > 30:
+            cleaned.append(l)
+        elif l[:2] == "==" and l[-2:] == "==":
+            cleaned.append(l)
+    text = "\n".join(cleaned)
+
+    # add paragraphs (identified by wiki section title which is always in format "==Some Title==")
+    text = text.replace("\n==", "\n\n\n==")
+
+    # remove empty paragrahps
+    text = re.sub(r"(==.*==\n\n\n)", "", text)
+
+    # remove multiple dashes
+    text = re.sub(r"#+ +", "", text)
+    text = re.sub(r" +#+", "", text)
+
+    return text
 
 
 def _get_document_store():
