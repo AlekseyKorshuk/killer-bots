1087bcf1e8a60a8c43e89200f46ce72cee92b863
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-31 13:03:16 +0300

    updates

--- test.py
+++ test.py
@@ -2,26 +2,21 @@ from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
+
+def get_score(text1, text2):
+    sentences = [text1, text2]
+    # Compute embeddings
+    embeddings = model.encode(sentences, convert_to_tensor=True)
+
+    # Compute cosine-similarities for each sentence with each other sentence
+    cosine_scores = util.cos_sim(embeddings, embeddings)
+
+    print(cosine_scores)
+
+
 # Single list of sentences
 sentences = ['#### During a code review',
              'The code review may be the last chance to tidy up the code before it becomes available to the public.',
-]
+             ]
 
-#Compute embeddings
-embeddings = model.encode(sentences, convert_to_tensor=True)
-
-#Compute cosine-similarities for each sentence with each other sentence
-cosine_scores = util.cos_sim(embeddings, embeddings)
-
-#Find the pairs with the highest cosine similarity scores
-pairs = []
-for i in range(len(cosine_scores)-1):
-    for j in range(i+1, len(cosine_scores)):
-        pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
-
-#Sort scores in decreasing order
-pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
-
-for pair in pairs[0:10]:
-    i, j = pair['index']
-    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], pair['score']))
+get_score(sentences[0], sentences[1])
