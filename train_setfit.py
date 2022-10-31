219adbe25ea887f43cf5012bae21a1e52a4f95c0
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-31 14:05:21 +0300

    updates

---
+++ train_setfit.py
@@ -0,0 +1,35 @@
+from datasets import load_dataset
+from sentence_transformers.losses import CosineSimilarityLoss
+
+from setfit import SetFitModel, SetFitTrainer
+
+# Load a dataset from the Hugging Face Hub
+dataset = load_dataset("AlekseyKorshuk/is_title")
+
+# Simulate the few-shot regime by sampling 8 examples per class
+num_classes = 2
+train_dataset = dataset["train"].shuffle(seed=42)
+eval_dataset = dataset["validation"]
+
+# Load a SetFit model from Hub
+model = SetFitModel.from_pretrained("sentence-transformers/paraphrase-mpnet-base-v2")
+
+# Create trainer
+trainer = SetFitTrainer(
+    model=model,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    loss_class=CosineSimilarityLoss,
+    metric="accuracy",
+    batch_size=16,
+    num_iterations=20,  # The number of text pairs to generate for contrastive learning
+    num_epochs=1,  # The number of epochs to use for constrastive learning
+    column_mapping={"sentence": "text", "label": "label"}  # Map dataset columns to text/label expected by trainer
+)
+
+# Train and evaluate
+trainer.train()
+metrics = trainer.evaluate()
+
+# Push model to the Hub
+trainer.push_to_hub("AlekseyKorshuk/is-title-setfit")
