7d5629a74da39f465c0eba81b9e49185000ff5e0
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-31 17:31:12 +0300

    updates

---
+++ killer_bots/search_engine/preprocess_docs.py
@@ -0,0 +1,102 @@
+import tqdm
+from haystack import Document
+from haystack.utils import convert_files_to_docs
+from sentence_transformers import SentenceTransformer, util
+from setfit import SetFitModel
+import re
+
+from killer_bots.search_engine.utils import change_extentions_to_txt
+
+
+def clean_wiki_text(text: str) -> str:
+    # get rid of multiple new lines
+    while "\n\n" in text:
+        text = text.replace("\n\n", "\n")
+
+    # add paragraphs (identified by wiki section title which is always in format "==Some Title==")
+    text = text.replace("\n==", "\n\n\n==")
+
+    # remove empty paragrahps
+    text = re.sub(r"(==.*==\n\n\n)", "", text)
+
+    # remove multiple dashes
+    text = re.sub(r"#+ +", "", text)
+    text = re.sub(r" +#+", "", text)
+
+    return text
+
+
+def get_docs_text(docs):
+    return '\n'.join([clean_wiki_text(doc.content) for doc in docs])
+
+
+def join_docs(docs):
+    return Document(content=get_docs_text(docs), meta=docs[0].meta)
+
+
+class PreprocessDocs:
+    def __init__(self):
+        self.similarity_model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.is_title_model = SetFitModel.from_pretrained("AlekseyKorshuk/is-title-setfit").to(0)
+        self.small_threshold = 0.1
+        self.threshold = 0.35
+        self.next_threshold = 0.4
+
+    def get_score(self, text1, text2):
+        text1 = clean_wiki_text(text1)
+        text2 = clean_wiki_text(text2)
+        embeddings = self.similarity_model.encode([text1, text2], convert_to_tensor=True)
+        cosine_scores = util.cos_sim(embeddings, embeddings)
+        return cosine_scores[0][1]
+
+    def is_title(self, text):
+        return self.is_title_model([clean_wiki_text(text)])[0] == 1
+
+    def split_last_titles(self, docs):
+        last_docs = []
+        doc = docs[-1]
+        while self.is_title(doc.content):
+            last_docs.append(doc)
+            docs.pop()
+            doc = docs[-1]
+        last_docs.reverse()
+        return docs, last_docs
+
+    def __call__(self, docs):
+        prepared_docs = []
+        current_docs = [docs[0]]
+        for i, doc in tqdm.tqdm(enumerate(docs[1:]), total=len(docs[1:]), title="Preprocessing docs"):
+            score = self.get_score(get_docs_text(current_docs), doc.content)
+            add_flag = False
+            if score > self.threshold and current_docs[-1].meta['name'] == doc.meta['name']:
+                add_flag = True
+            elif self.is_title(get_docs_text(current_docs)) and score > self.small_threshold:
+                add_flag = True
+            else:
+                next_score = self.get_score(
+                    get_docs_text(current_docs) + '\n' + doc.content,
+                    docs[i + 1].content
+                )
+                if next_score > self.next_threshold and current_docs[-1].meta['name'] == docs[i + 1].meta['name']:
+                    add_flag = True
+            if add_flag:
+                current_docs.append(doc)
+                if i == len(docs) - 2:
+                    prepared_docs.append(join_docs(current_docs))
+            else:
+                current_docs, last_docs = self.split_last_titles(current_docs)
+                prepared_docs.append(join_docs(current_docs))
+                current_docs = last_docs + [doc]
+        return prepared_docs
+
+
+if __name__ == "__main__":
+    doc_dir = './killer_bots/bots/code_guru/database/'
+    change_extentions_to_txt(doc_dir)
+    docs = convert_files_to_docs(dir_path=doc_dir, clean_func=None, split_paragraphs=True)
+    preprocessor = PreprocessDocs()
+    prepared_docs = preprocessor(docs)
+
+    for doc in prepared_docs:
+        print(doc)
+        print("#" * 100)
