c7ae578c9801ee971bc0af046236cccc62e6e047
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-31 19:58:47 +0300

    updates

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ import numpy as np
 from transformers import pipeline
 
 import torch
+
 print(f"Is CUDA available: {torch.cuda.is_available()}")
 print(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
 
-pipe_flan = pipeline("text2text-generation", model="google/flan-t5-xl", device="cuda:0", model_kwargs={"torch_dtype":torch.bfloat16})
+pipe_flan = pipeline("text2text-generation", model="google/flan-t5-xl", device="cuda:0",
+                     model_kwargs={"torch_dtype": torch.bfloat16})
 
 with open('./killer_bots/bots/code_guru/database/handwritten.txt', 'r') as f:
     text = f.read()
@@ -17,4 +19,5 @@ paragraphs = [p for p in paragraphs if len(p) > 0]
 
 for p in paragraphs:
     print(f"Paragraph: {p}")
-    print(f"Response: {pipe_flan(f'Summarize the following text: {p}', max_length=4096, num_beams=10, num_return_sequences=1)}")
+    print(f"Response: {pipe_flan(f'Summarize the following text: {p}', max_length=4096)[0]['generated_text']}")
+    print()
