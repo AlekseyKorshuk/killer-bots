b8d6fdd8a7b6e67b1705bb078a03c5ace6dd65c3
Aleksey Korshuk <ale-kor02@mail.ru>
2022-10-31 13:49:35 +0300

    updates

---
+++ collect_data_is_title.py
@@ -0,0 +1,46 @@
+import tqdm
+from haystack.utils import convert_files_to_docs
+from transformers import AutoTokenizer, AutoModel
+import torch
+import torch.nn.functional as F
+from nltk import sent_tokenize
+from sentence_transformers import SentenceTransformer, util
+import datasets
+# Sentences we want sentence embeddings for
+from killer_bots.search_engine.custom_pipeline import clean_wiki_text
+from killer_bots.search_engine.utils import change_extentions_to_txt
+
+doc_dir = './killer_bots/bots/code_guru/database/'
+
+change_extentions_to_txt(doc_dir)
+docs = convert_files_to_docs(dir_path=doc_dir, clean_func=None, split_paragraphs=True)
+sentences = [doc.content.strip() for doc in docs]
+
+# data = [
+#     {
+#         'text': clean_wiki_text(sentence),
+#         'label': 1 if str(sentence).startswith("#") else 0
+#     } for sentence in sentences
+# ]
+
+from sklearn.model_selection import train_test_split
+
+
+
+data = {
+    "text": [clean_wiki_text(sentence) for sentence in sentences],
+    "label": [1 if str(sentence).startswith("#") else 0 for sentence in sentences]
+}
+
+X = data['text']
+y = data['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    stratify=y,
+                                                    test_size=0.25)
+
+
+print('% of positive samples in train set:', sum(y_train== 1) / len(X_train))
+print('% of positive samples in test set:', sum(y_test== 1) / len(X_test))
+
+# dataset = datasets.Dataset.from_dict(data)
