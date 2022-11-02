90bbf05aa6d8e0cd5c0f041fe8cb0e7b76275412
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-02 21:23:00 +0300

    updates

--- killer_bots/search_engine/web_parser.py
+++ killer_bots/search_engine/web_parser.py
@@ -1,6 +1,12 @@
 import article_parser
 
 from killer_bots.search_engine.preprocess_docs import clean_wiki_text
+from haystack.nodes import TransformersSummarizer
+from haystack import Document
+
+
+
+summarizer = TransformersSummarizer(model_name_or_path="google/pegasus-xsum")
 
 url = "https://blog.unosquare.com/10-tips-for-writing-cleaner-code-in-any-programming-language"
 
@@ -10,11 +16,18 @@ title, content = article_parser.parse(
     timeout=5
 )
 
-print(content)
+# print(content)
 content = clean_wiki_text(content)
 
 docs = content.split("\n")
 docs = [doc.strip() for doc in docs]
-docs = [doc for doc in docs if len(doc) > 0]
+docs = [Document(doc) for doc in docs if len(doc) > 0]
 
 print(len(docs))
+
+summary = summarizer.predict(documents=docs, generate_single_summary=True)
+print(summary)
+
+
+summary = summarizer.predict(documents=docs[:1], generate_single_summary=True)
+print(summary)
