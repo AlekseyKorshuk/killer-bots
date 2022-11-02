80c522aede25f649522385a2bdcf8a12a1c91732
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-02 14:34:23 +0300

    updates

--- rag_test.py
+++ rag_test.py
@@ -73,4 +73,7 @@ def article_text_extractor(url: str):
 article_header, chunks = article_text_extractor("https://blog.unosquare.com/10-tips-for-writing-cleaner-code-in-any-programming-language")
 
 print(len(chunks))
-print(chunks)
+
+for chunk in chunks:
+    print(chunk)
+    print()
