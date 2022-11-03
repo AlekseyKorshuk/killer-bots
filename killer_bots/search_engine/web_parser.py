26f619143acea843554f9210c7ea3fd080660bca
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-03 15:52:17 +0300

    updates

--- killer_bots/search_engine/web_parser.py
+++ killer_bots/search_engine/web_parser.py
@@ -140,7 +140,7 @@ class GoogleSearchEngine:
     def _get_docs(self, content, html):
         docs = content.split("\n")
         docs = [doc.strip() for doc in docs]
-        docs = [doc for doc in docs if len(doc) > 10]
+        docs = [doc for doc in docs if len(doc) > 30]
         docs = [clean_wiki_text(doc) for doc in docs]
         docs = [Document(doc, meta={"name": None}) for doc in docs]
         print("From:", len(docs))
