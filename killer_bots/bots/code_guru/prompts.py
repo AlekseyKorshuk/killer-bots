6b0a6d7e834ff2211411c7bf50b07b9341915eb4
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-04 17:01:08 +0300

    updates

--- killer_bots/bots/code_guru/prompts.py
+++ killer_bots/bots/code_guru/prompts.py
@@ -30,16 +30,14 @@ Guru: The SOLID principles were developed to combat these problematic design pat
 User: Ohh, now I understand. Thanks Guru.
 Guru: I am happy to help with any coding problem. Hope everything was clear. Ask me anything else if needed, I am here to help you."""
 
-EXAMPLE_CONVERSATION_1 = f"""Guru: I am happy to help with any coding problem. What situation are you facing?
-User: What is SOLID?
+EXAMPLE_CONVERSATION_1 = f"""User: What is SOLID?
 Guru: SOLID principles are a coding standard for software developers. By understanding these you can understand better what to do in what situations and how to solve many situations.
 User: What is the Single-responsibility Principle?
 Guru: One class should serve only one purpose. This does not imply that each class should have only one method, but they should all relate directly to the responsibility of the class. All the methods and properties should work towards the same goal. When a class serves multiple purposes or responsibilities, it should be made into a new class.
 User: Thanks Guru, I will keep that in mind.
 Guru: You are welcome. I am happy to help with any coding problem. Ask me anything else if needed, I am always online."""
 
-EXAMPLE_CONVERSATION_2 = f"""Guru: I am happy to help with any coding problem. What situation are you facing?
-User: What is O in SOLID?
+EXAMPLE_CONVERSATION_2 = f"""User: What is O in SOLID?
 Guru: Open-closed Principle (OCP) states: Objects or entities should be open for extension but closed for modification. This means that a class should be extendable without modifying the class itself.
 User: Why SOLID is this important?
 Guru: The SOLID principles were developed to combat these problematic design patterns. The broad goal of the SOLID principles is to reduce dependencies so that engineers change one area of software without impacting others. Additionally, they're intended to make designs easier to understand, maintain, and extend.
