0a5179439cac7239340410b49e94d160819e9a62
Aleksey Korshuk <ale-kor02@mail.ru>
2022-11-05 15:12:28 +0300

    updates

--- killer_bots/bots/therapist/utils.py
+++ killer_bots/bots/therapist/utils.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import pandas as pd
+import tqdm
 from summarizer import Summarizer
 from transformers import AutoTokenizer
 
@@ -12,14 +13,14 @@ def get_chats_from_db():
     max_tokens = 128
     tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b")
     chats = []
-    for i, row in df.iterrows():
+    for i, row in tqdm.tqdm(df.iterrows(), total=len(df)):
         answer = row['answerText']
         num_tokens = len(tokenizer(answer).input_ids)
         ratio = min(max_tokens / num_tokens, 1)
         if ratio != 0:
             answer = summarizer(answer, ratio=ratio)
-        question_title = row['questionTitle'] if row['questionTitle'] else ""
-        question_text = row['questionText'] if row['questionText'] else ""
+        question_title = str(row['questionTitle']) if row['questionTitle'] else ""
+        question_text = str(row['questionText']) if row['questionText'] else ""
         chats.append(
             f"User: {question_title + question_text}\n"
             f"Therapist: {answer}"
